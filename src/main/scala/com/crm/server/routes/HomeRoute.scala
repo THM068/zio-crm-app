3210517b006237f4d5ce4fc773286cb50d843e17
Thando Mafela <thando.mafela@sky.uk>
2023-12-22 11:16:53 +0000

    clean up

--- src/main/scala/com/crm/server/routes/HomeRoute.scala
+++ src/main/scala/com/crm/server/routes/HomeRoute.scala
@@ -17,20 +17,11 @@ class HomeRoute() {
     ViewRenderer.render(content.body)
   }
 
-  val assets = Method.GET / "assets"  ->
-    Handler.fromFile(new File(s"src/main/resources/web/assets/custom.css"))
 
-  def handlerFromFile(filename: String): Handler[Any, Throwable, Any, Response] = {
-    println(filename)
-    Handler
-      .fromFile(new File
-      (s"src/main/resources/web/assets/${filename}"))
-  }
-
-    val apps: HttpApp[Any] = Routes(index, assets)
-      .handleError(handle)
-      .sandbox
-      .toHttpApp
+  val apps: HttpApp[Any] = Routes(index)
+    .handleError(handle)
+    .sandbox
+    .toHttpApp
 
     def handle(throwable: Throwable) = {
       Response.text("The error is " + throwable).status(Status.InternalServerError)
